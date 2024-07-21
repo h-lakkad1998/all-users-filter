baeeb34b798b240f9c44b2923a34a0eaa9b5790d
h-lakkad1998 <hardiklakkad2@gmail.com>
2024-07-21 15:32:14 +0530

    text domain issue solved

--- inc/admin/class.lkd_main.php
+++ inc/admin/class.lkd_main.php
@@ -50,10 +50,14 @@ class LKD_USERS_FILTER {
 
     private function array_to_csv_download($query_array = array(array("maybe an issue!")), $filename = "export_users.csv", $delimiter = ",")
     {
+        $check_filetype = wp_check_filetype($filename);
+        // check only able to process csv file
+        if( 'csv' !== $check_filetype['ext'] ){
+            wp_die( 'File type not allowed :( ' );
+        }
         if ( !function_exists('request_filesystem_credentials') ) {
             require_once(ABSPATH . 'wp-admin/includes/file.php');
         }
-        
         $creds = request_filesystem_credentials('', '', false, false, null);
         if ( !WP_Filesystem($creds) ) {
             $query_array = array( array("Could not access filesystem.") );
@@ -61,46 +65,38 @@ class LKD_USERS_FILTER {
         }
         global $wp_filesystem;
         $temp_file = wp_tempnam();
-
         if (!$temp_file) {
             $query_array = array( array("Could not create temporary file.") );
             return $query_array;
         }
-
         // Prepare CSV data
         $csv_data = '';
         foreach ($query_array as $line) {
             $csv_data .= implode($delimiter, $line) . "\r\n";
         }
-
         // Write CSV data to the temporary file using WP_Filesystem
         if (!$wp_filesystem->put_contents($temp_file, $csv_data, FS_CHMOD_FILE)) {
             $query_array = array( array("Could not write to temporary file.") );
             return $query_array;
         }
-
         // Read the content of the temporary file using WP_Filesystem
         $csv_content = $wp_filesystem->get_contents($temp_file);
-
         if ($csv_content === false) {
             $query_array = array( array("Could not read temporary file.") );
             return $query_array;
         }
-
         // Clean up the temporary file
         $wp_filesystem->delete($temp_file);
-
         // Set headers and output the CSV content
         header('Content-Type: text/csv');
-        header('Content-Disposition: attachment; filename="' . esc_attr($filename) . '";');
+        header('Content-Disposition: attachment; filename="' . sanitize_file_name($filename) . '";');
         header('Content-Length: ' . strlen($csv_content));
-
         // Output CSV content with proper escaping
         echo wp_kses_post($csv_content);
-
         exit;
     }
 }
+
 add_action('plugins_loaded', function () {
     if (current_user_can('administrator')) {
         $lkd_user_obj = new LKD_USERS_FILTER();
